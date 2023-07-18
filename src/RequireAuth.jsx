3bcc6ef16f5f6d5dbccbf294504453fedaf78d8b
Abd eldjalil <selamnia.a.eldjalil@gmail.com>
2023-07-18 20:02:46 +0100

    login redirect

--- src/RequireAuth.jsx
+++ src/RequireAuth.jsx
@@ -1,11 +1,12 @@
 import React, { useEffect, useState } from "react"
-import { Navigate } from "react-router-dom"
+import { Navigate, useParams } from "react-router-dom"
 import { Outlet } from "react-router-dom"
 
 
 export default function RequireAuth(){
     
     const isLoggedIn = localStorage.getItem("loggedin") === "true" ? true : false
+
     
     if(!isLoggedIn){
         return <Navigate to="/login" state={{message: "You must Login first!"}}/>
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,55 +1,51 @@
 import React, { useState } from "react";
-import { Link, useLocation, Form } from "react-router-dom";
+import { Link, useLocation, Form, useActionData, Navigate, useNavigation } from "react-router-dom";
 import { loginUser } from "../../api";
 import './Login.css'
 
 
 export async function action({ request }) {
 
+    let redirect = false
     const formData = await request.formData()
     const email = formData.get("email")
     const password = formData.get("password")
-    const data = await loginUser({email, password})
-    .then(data => data.token ? localStorage.setItem("loggedin", true) : null)
-    .then(() => {rredirect = true})
-        .catch(err => console.log(err))
-        
-    
-    console.log(localStorage.getItem("loggedin"))
-    return null
+    try{
+        const data = await loginUser({email, password})
+        .then(data => data.token ? localStorage.setItem("loggedin", true) : null)
+        .then(redirect = true)
+        return redirect
+    } catch(err){
+        return  err.message
+    }
 }
 
 export default function Login(){
 
 
-    const [status, setStatus] = useState("idle")
-    const [error, setError] = useState(null)
     const  { state } = useLocation()
+    const redirectErr = useActionData()
+    
     const messagee = state
+    const navigation = useNavigation().state
+    
 
-
-    function handleSubmit(event){
-        messagee.message = ""
-        event.preventDefault()
-        setStatus("submitting")
-        setError(null)
-        .finally(() => setStatus("idle"))
-
-    }
+  
     
 
 
     return (
         <div className="login--wrapper">
+            {redirectErr === true && <Navigate to="/host" />}
             <div className="login--container">
                 <h1>Sign in to your account</h1>
-                {error && <h2>{error.message}</h2>}
-                {messagee?.message && <h2>{messagee.message}</h2>}
+                {messagee && <h2>{messagee.message}</h2>}
+                {redirectErr && <h2>{redirectErr}</h2>}
             <Form method="post" className="login--form" replace>
                 <input name="email" className="login--input" type="email" placeholder="Enter your email" />
                 <input name="password" className="login--input under" type="password" placeholder="Enter your password" />
-                <button disabled={status === "submitting" ? true : false} >
-                    Sign in
+                <button disabled={navigation === "submitting"} >
+                    {navigation === "idle" ? "Sign in" : "Logging In..."}
                 </button>
             </Form>
             <section>Don't have an account? <Link to='.' className="createaccount">Create one now</Link></section>
